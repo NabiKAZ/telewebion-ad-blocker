799a5fe77865cef5a2b672c400a30e5769169071
Nabi KaramAliZadeh <NabiKAZ@gmail.com>
2022-12-04 01:24:14 +0330

    Create Telewebion.com_Ad_Blocker.user.js

---
+++ Telewebion.com_Ad_Blocker.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Telewebion.com Ad Blocker
+// @namespace    https://github.com/NabiKAZ/telewebion-ad-blocker
+// @version      0.1
+// @description  Telewebion.com Website Ad Blocker
+// @author       @NabiKAZ
+// @match        https://www.telewebion.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var interval = setInterval(function() {
+
+        var el = document.getElementsByClassName('vast-skip-button')[0];
+        if (el) {
+            el.style.display = 'block';
+            el.classList.add("enabled");
+            el.click();
+            clearInterval(interval);
+        } else {
+            return;
+        }
+
+    }, 500);
+
+})();
